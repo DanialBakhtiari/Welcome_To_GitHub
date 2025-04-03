9f582ba27976f27698a0b97ff295ade33bf77ca0
Danial Bakhtiari <danial.bakhtiari96@gmail.com>
2025-04-03 12:18:56 +0330

    Start Code

---
+++ src/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 12,
+    spaceBetween: 1,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
